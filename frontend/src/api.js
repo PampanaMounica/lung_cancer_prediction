c50c086adc68e69f36407dc8f7c054b6c9b8c450
Pampana Mounica <pampanamounica@gmail.com>
2026-01-02 22:58:40 +0530

    Configured API URL for Render deployment

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:5000",
+  baseURL: process.env.REACT_APP_API_URL,
 });
+
 
 export const predictCancer = (data) => api.post("/predict", data);
